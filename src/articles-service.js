a5a362bdcdf3cb1c60eaa6b1cdda13777a0dfa57
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2019-12-11 15:00:58 -0500

    testing done

--- src/articles-service.js
+++ src/articles-service.js
@@ -15,6 +15,16 @@ const ArticlesService = {
        getById(knex, id) {
           return knex.from('blogful_articles').select('*').where('id', id).first()
         },
+        deleteArticle(knex, id) {
+            return knex('blogful_articles')
+              .where({ id })
+              .delete();
+          },
+          updateArticle(knex, id, newArticleFields) {
+            return knex('blogful_articles')
+              .where({ id })
+              .update(newArticleFields);
+          },
 }
 
 
